abb7e1540cc51266fb60d0a77cfec68e8c6f4c3a
unknown <demeyer.jeroen@hotmail.com>
2015-08-31 19:39:47 +0200

    Added choiceBox to configure the playingField

--- src/mastermind/PlayingFieldCompanion.java
+++ src/mastermind/PlayingFieldCompanion.java
@@ -1,7 +1,10 @@
 package mastermind;
 
+import java.util.ArrayList;
+import java.util.List;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -32,8 +35,24 @@ public class PlayingFieldCompanion implements InvalidationListener{
         }
     }
     
+    public void initialize(){
+        colors.setItems(FXCollections.observableArrayList(
+                createOptions(6,11)));
+        codeLength.setItems(FXCollections.observableArrayList(
+                createOptions(4,9)));
+    }
+    
+    @Override
     public void invalidated(Observable o){
         
+    }
+    
+    public List<Integer> createOptions(int begin, int end){
+        List<Integer> result = new ArrayList<>();
+        for(int i=begin; i<=end; i++){
+            result.add(i);
+        }
+        return result;
     }
     
 }
